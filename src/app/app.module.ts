97d2aa81142d3928e2bc309e221cd7083a4e1800
John Reese <jpreese@mtu.edu>
2018-08-30 22:12:09 -0400

    Add role selector component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoleSelectorComponent } from './roleselector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoleSelectorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/roleselector.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'role-selector',
+    templateUrl: 'roleselector.component.html'
+})
+
+export class RoleSelectorComponent { }
